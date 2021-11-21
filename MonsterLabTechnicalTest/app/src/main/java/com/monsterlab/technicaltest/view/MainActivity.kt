494afa92561dc015686ae19c1eff7916c7a20008
Ali Ahmed <ali.ahmed@bracbank.com>
2021-11-21 13:17:30 +0600

    Project structure has been created

---
+++ MonsterLabTechnicalTest/app/src/main/java/com/monsterlab/technicaltest/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.monsterlab.technicaltest.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.monsterlab.technicaltest.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
